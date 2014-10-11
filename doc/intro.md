742de04f5068946e9f3814fcbb1ff9bde1f4d960
Chris Oei <xcko-git@yahoo.com>
2014-10-10 17:26:29 -0700

    A  .gitignore A  LICENSE A  README.md A  doc/intro.md A  project.clj A  src/contemporary_words/core.clj A  test/contemporary_words/core_test.clj

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to contemporary-words
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/contemporary_words/core_test.clj
@@ -0,0 +1,7 @@
+(ns contemporary-words.core-test
+  (:require [clojure.test :refer :all]
+            [contemporary-words.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
